1c2c30719eac5589b2b35bd74cc83089acb97474
snorlax-98m <Anupamdas19980@gmail.com>
2023-05-02 04:51:52 +0530

    most api's created kal dekehenge

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,22 @@ const userSchema = new Schema({
   password: String,
 });
 mongoDB();
+/* `app.use(cors({ origin: "*" }));` is setting up Cross-Origin Resource Sharing (CORS) for the Express
+app. CORS is a security feature implemented in web browsers that restricts web pages from making
+requests to a different domain than the one that served the web page. By setting `origin: "*"`, the
+server is allowing requests from any domain to access its resources. This is useful when building
+APIs that need to be accessed by different domains or when developing locally and testing with
+different front-end applications. */
 app.use(cors({ origin: "*" }));
+/* `app.use(express.json());` is a middleware function in Express that parses incoming requests with
+JSON payloads. It basically allows the server to accept JSON data in the request body and parse it
+into a JavaScript object that can be used in the server-side code. */
 app.use(express.json());
 
+/* This code is defining a route for a GET request to the root endpoint ("/"). When a GET request is
+made to this endpoint, it creates a model for the "pizza" collection using the `mongoose.model()`
+method, and then calls the `find()` method on the model to retrieve all the documents in the
+collection. Finally, it sends a JSON response with the retrieved data. */
 app.get("/", (req, res) => {
   const pizza = mongoose.model("pizza", dataSchema);
   pizza.find({}).then((data) => {
@@ -28,6 +41,12 @@ app.get("/", (req, res) => {
   });
 });
 
+/* This code is defining a route for getting all the documents in the "pizza" collection in the MongoDB
+database where the "categories" field is equal to "veg". When a GET request is made to the "/veg"
+endpoint, it creates a model for the "pizza" collection using the `mongoose.model()` method, and
+then calls the `find()` method on the model with a query object to retrieve all the documents in the
+collection where the "categories" field is equal to "veg". Finally, it sends a JSON response with
+the retrieved data. */
 app.get("/veg", (req, res) => {
   const pizza = mongoose.model("pizza", dataSchema);
   pizza.find({ categories: "veg" }).then((data) => {
@@ -35,6 +54,11 @@ app.get("/veg", (req, res) => {
   });
 });
 
+/* This code is defining a route for adding a new document to the "Cart" collection in the MongoDB
+database. When a POST request is made to the "/cart" endpoint, it creates a model for the "Cart"
+collection using the `mongoose.model()` method, and then calls the `create()` method on the model to
+add a new document to the collection with the data provided in the request body. Finally, it sends a
+response with the message "send". */
 app.post("/cart", (req, res) => {
   const cart = mongoose.model("Cart", dataSchema);
   const { name, price, categories, imgUrl } = req.body;
@@ -49,6 +73,12 @@ app.post("/cart", (req, res) => {
       res.send("send");
     });
 });
+/* This code is defining a route for handling a POST request to the "/login" endpoint. When a POST
+request is made to this endpoint, it creates a model for the "user" collection using the
+`mongoose.model()` method, and then calls the `create()` method on the model to add a new document
+to the collection with the data provided in the request body (name, email, and password). Finally,
+it sends a JSON response with the message "successful registration". This code is used for
+registering a new user in the application. */
 app.post("/login", (req, res) => {
   const user = mongoose.model("user", userSchema);
   const { name, email, password } = req.body;
@@ -62,6 +92,11 @@ app.post("/login", (req, res) => {
       res.json("sucessfull regiestration");
     });
 });
+/* This code is defining a route for getting all the documents in the "user" collection in the MongoDB
+database. When a GET request is made to the "/login" endpoint, it creates a model for the "user"
+collection using the `mongoose.model()` method, and then calls the `find()` method on the model to
+retrieve all the documents in the collection. Finally, it sends a JSON response with the retrieved
+data. */
 app.get("/login", (req, res) => {
   const user = mongoose.model("user", userSchema);
   user.find({}).then((data) => {
@@ -69,10 +104,26 @@ app.get("/login", (req, res) => {
   });
 });
 
+/* This code is defining a route for getting all the documents in the "Cart" collection in the MongoDB
+database. When a GET request is made to the "/cart" endpoint, it creates a model for the "Cart"
+collection using the `mongoose.model()` method, and then calls the `find()` method on the model to
+retrieve all the documents in the collection. Finally, it sends a JSON response with the retrieved
+data. */
 app.get("/cart", (req, res) => {
   const cart = mongoose.model("Cart", dataSchema);
   cart.find({}).then((data) => {
     res.json(data);
+  });
+});
+/* This code is defining a route for deleting all the documents in the "Cart" collection in the MongoDB
+database. When a DELETE request is made to the "/cart" endpoint, it creates a model for the "Cart"
+collection using the `mongoose.model()` method, and then calls the `deleteMany()` method on the
+model to delete all the documents in the collection. Finally, it sends a JSON response with the
+message "data". */
+app.delete("/cart", (req, res) => {
+  const cart = mongoose.model("Cart", dataSchema);
+  cart.deleteMany({}).then((data) => {
+    res.json("data");
   });
 });
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,40 @@
+import axios from "axios";
 import React from "react";
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "../app.css";
 
 const Cart = () => {
-  return <div></div>;
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/cart").then((res) => {
+      setdata(res.data);
+    });
+  }, []);
+  return (
+    <>
+      <Header />
+      <div className="cart-card-group card-group">
+        {data.map((item) => {
+          return (
+            <div className="card">
+              <img src={item.imgUrl} className="card-img" alt="..." />
+              <h5 className="card-title"></h5>
+              <div className="btn-group">
+                <h5>{item.name} </h5>
+                <div className="button-div">
+                  <button className="button">Add to cart</button>
+                  <button className="button">Remove</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Cart;
